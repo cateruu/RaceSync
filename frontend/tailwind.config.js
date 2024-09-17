32dc15039f55b116990ca85c38485754b2290320
cateruu <cater981pl@gmail.com>
2024-09-17 11:46:54 +0200

    change to wails2

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      azeretMono: ['Azeret Mono', 'sans-serif'],
+      fasterOne: ['Faster One', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
